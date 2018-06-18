af583c5d300ac614bc065250860f8ae178b6d20b
Rodrigo Martins <rodrigomlima@outlook.com>
2018-06-18 10:36:02 -0300

    Atualização de arquivo.c
    Adição de comentários e remoção de itens desencessários

--- arquivo.c
+++ arquivo.c
@@ -1,28 +1,28 @@
 #include "arquivo.h"
 
-// Declaração das funções
+// DeclaraÃ§Ã£o das funÃ§Ãµes
 
-// Função para gerar números reais aleatórios
+// FunÃ§Ã£o para gerar nÃºmeros reais aleatÃ³rios
 double real_aleatorio()
 {
-    // A multiplicação por 1000 faz com que se gere números reais de 0 a 1000
+    // A multiplicaÃ§Ã£o por 1000 faz com que se gere nÃºmeros reais de 0 a 1000
     return (((double)rand() / (double)RAND_MAX) * 1000);
 }
 
-// Função para arrumar os nomes dos arquivos
+// FunÃ§Ã£o para arrumar os nomes dos arquivos
 void arruma_nome(char arq[])
 {
 	arq[strcspn(arq, "\n")] = 0; // Tira o '\n' da string
-    strcat(arq, ".txt"); // Adiciona a extensão .txt do nome
+    strcat(arq, ".txt"); // Adiciona a extensÃ£o .txt do nome
 }
 
-// Função que cria o arquivo
+// FunÃ§Ã£o que cria o arquivo
 void cria_arquivo(int tam, char arq[100])
 {
-    // Garante que o número será aleatório
+    // Garante que o nÃºmero serÃ¡ aleatÃ³rio
     srand((unsigned)time(NULL));
 
-    // Declaração de variáveis
+    // DeclaraÃ§Ã£o de variÃ¡veis
     int i;
     double valor;
     FILE *vetor_real; // Arquivo
@@ -30,7 +30,7 @@ void cria_arquivo(int tam, char arq[100])
     // Abre o arquivo
     vetor_real = fopen(arq, "w");
 
-    // Cria valores aleatórios e salva no arquivo
+    // Cria valores aleatÃ³rios e salva no arquivo
     for (i = 0; i < tam; i++)
     {
         valor = real_aleatorio();
@@ -41,10 +41,10 @@ void cria_arquivo(int tam, char arq[100])
     fclose(vetor_real);
 }
 
-// Função que carrega os valores do arquivo num vetor
+// FunÃ§Ã£o que carrega os valores do arquivo num vetor
 int carrega_vetor(int tam, double vetor[tam], char arq[100])
 {
-	// Declaração de variáveis	
+	// DeclaraÃ§Ã£o de variÃ¡veis	
 	int i;
 	FILE *vetor_real; // Arquivo
 
@@ -63,21 +63,22 @@ int carrega_vetor(int tam, double vetor[tam], char arq[100])
 	return 0;
 }
 
+// FunÃ§Ã£o para salvar o vetor num arquivo
 void salva_arquivo(int tam, double vetor[tam], char arq[100])
 {
-	// Declaração de variáveis	
+	// DeclaraÃ§Ã£o de variÃ¡veis	
 	int i;
-    FILE *saida; // Arquivo
+    	FILE *saida; // Arquivo
 
-    // Abre o arquivo
-    saida = fopen(arq, "w");
+    	// Abre o arquivo
+    	saida = fopen(arq, "w");
 
-    // Cria o vetor aleatório e salva no arquivo
-    for (i = 0; i < tam; i++)
-    {
-        fprintf(saida, "%f ", vetor[i]);
-    }
+    	// Cria o vetor aleatÃ³rio e salva no arquivo
+    	for (i = 0; i < tam; i++)
+    	{
+        	fprintf(saida, "%f ", vetor[i]);
+    	}
 
-    // Fecha o arquivo
-    fclose(saida);
+    	// Fecha o arquivo
+    	fclose(saida);
 }
